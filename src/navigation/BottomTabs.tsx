5978f8c9cf051642892b6367e1f73594af02a040
栗田孟 <akito14140099@gmail.com>
2025-10-17 10:34:22 +0900

    画面下部のボタン修正

--- src/navigation/BottomTabs.tsx
+++ src/navigation/BottomTabs.tsx
@@ -2,6 +2,7 @@
 // React Navigation Bottom Tabs
 
 import React from 'react';
+import { Image } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/HomeScreen';
 import InputScreen from '../screens/InputScreen';
@@ -13,12 +14,32 @@ const Tab = createBottomTabNavigator();
 
 const BottomTabs = () => {
   return (
-    <Tab.Navigator screenOptions={{ headerShown: false }}>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Input" component={InputScreen} />
-      <Tab.Screen name="Insight" component={InsightScreen} />
-      <Tab.Screen name="Goals" component={GoalsScreen} />
-      <Tab.Screen name="MyPage" component={MyPageScreen} />
+    <Tab.Navigator screenOptions={{ headerShown: false, tabBarActiveTintColor: '#0076FF', tabBarInactiveTintColor: '#8C8C8C' }}>
+      <Tab.Screen name="Home" component={HomeScreen} options={{
+        tabBarIcon: ({ size, color }) => (
+          <Image source={require('../icons/hime.png')} style={{ width: size, height: size, tintColor: color }} />
+        )
+      }} />
+      <Tab.Screen name="Input" component={InputScreen} options={{
+        tabBarIcon: ({ size, color }) => (
+          <Image source={require('../icons/data2.png')} style={{ width: size, height: size, tintColor: color }} />
+        )
+      }} />
+      <Tab.Screen name="Insight" component={InsightScreen} options={{
+        tabBarIcon: ({ size, color }) => (
+          <Image source={require('../icons/search.png')} style={{ width: size, height: size, tintColor: color }} />
+        )
+      }} />
+      <Tab.Screen name="Goals" component={GoalsScreen} options={{
+        tabBarIcon: ({ size, color }) => (
+          <Image source={require('../icons/goal2.png')} style={{ width: size, height: size, tintColor: color }} />
+        )
+      }} />
+      <Tab.Screen name="MyPage" component={MyPageScreen} options={{
+        tabBarIcon: ({ size, color }) => (
+          <Image source={require('../icons/bel2.png')} style={{ width: size, height: size, tintColor: color }} />
+        )
+      }} />
     </Tab.Navigator>
   );
 };
